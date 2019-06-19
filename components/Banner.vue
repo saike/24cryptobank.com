29c00338f2d05d21961270df7f42b5af3dbdeeb3
saike <kishkoglot@gmail.com>
2019-06-20 02:18:19 +0300

    png images fix

--- components/Banner.vue
+++ components/Banner.vue
@@ -129,7 +129,7 @@
           {
             title: 'ОНЛАЙН',
             subtitle: 'за 10 минут',
-            background: 'url(/banners/online.jpg) no-repeat bottom right/cover',
+            background: 'url(/banners/online.png) no-repeat bottom right/cover',
             align: 'right',
             benefits: [
               'безопасный перевод ·',
@@ -143,7 +143,7 @@
           {
             title: 'за наличные в офисе',
             subtitle: '"МОСКВА-СИТИ"',
-            background: 'url(/banners/ofice.jpg) no-repeat bottom right/cover',
+            background: 'url(/banners/ofice.png) no-repeat bottom right/cover',
             align: 'left',
             benefits: [
               '· офис в центре Москвы',
