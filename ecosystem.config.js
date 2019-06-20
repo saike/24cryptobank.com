c0aa36942b0a1683504c261956beb9cc70364dcf
saike <kishkoglot@gmail.com>
2019-06-21 01:54:41 +0300

    pm2 fix 2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps : [{
     name      : '24cryptobank.com', // App name that shows in `pm2 ls`
-    exec_mode : 'cluster', // enables clustering
-    instances : 'max', // or an integer
+    exec_mode : 'fork', // enables clustering
+    instances : 1, // or an integer
     // cwd       : './', // only if using a subdirectory
-    script    : './node_modules/nuxt/bin/nuxt-start', // The magic key
+    script    : './node_modules/nuxt/bin/nuxt.js', // The magic key
   }]
 };
